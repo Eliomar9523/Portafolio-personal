2fe3862bdadd655f69672c4ef384c079d00eba9c
Eliomar9523 <elioorh1001@gmail.com>
2026-04-30 19:22:41 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -50,17 +50,17 @@ function abrirFacebook() {
   var urlApp = "fb://profile/eliomar.reyes.7587"; 
   var urlWeb = "https://www.facebook.com/eliomar.reyes.7587";
 
-  // Detecta si es móvil
+  
   if (/Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-    // En móvil intenta abrir la app
+    
     window.location.href = urlApp;
 
-    // Si no abre la app, después de 1 segundo abre la versión web
+   
     setTimeout(function() {
       window.location.href = urlWeb;
     }, 1000);
   } else {
-    // En PC/laptop siempre abre nueva pestaña
+    
     window.open(urlWeb, "_blank");
   }
 }
@@ -70,17 +70,17 @@ function abrirInstagram() {
   var urlApp = "instagram://user?username=eliomar.reyes.75873"; 
   var urlWeb = "https://www.instagram.com/eliomar.reyes.75873/";
 
-  // Detecta si es móvil
+  
   if (/Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) {
-    // En móvil intenta abrir la app
+    
     window.location.href = urlApp;
 
-    // Si no abre la app, después de 1 segundo abre la versión web
+    
     setTimeout(function() {
       window.location.href = urlWeb;
     }, 1000);
   } else {
-    // En PC/laptop siempre abre nueva pestaña
+   
     window.open(urlWeb, "_blank");
   }
 }
@@ -90,17 +90,17 @@ const modal = document.getElementById("miModal");
 const btn = document.getElementById("btnAbrir");
 const span = document.querySelector(".close-btn");
 
-// Al hacer click, se muestra el modal
+
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Al hacer click en la X, se oculta
+
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Si el usuario hace click fuera de la caja, también se cierra
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
